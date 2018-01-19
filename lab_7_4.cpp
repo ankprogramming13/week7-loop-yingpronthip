f131418f169e7c54369f8dd27509c725b425608a
Unknown <pronthipza@gmail.com>
2018-01-19 14:00:32 +0700

    ข้อ 4

--- lab_7_4.cpp
+++ lab_7_4.cpp
@@ -4,40 +4,27 @@ using namespace std;
 
 int main () {
 
+    // Local variable declaration:
+    int a = 0;
+
+    // while loop execution
+    while( a < 3 ) {
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+      a++;
+    }
+
   // Without loop
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
+
 
   // เขียนโปรแกรมให้ทำงานได้ตามข้างต้นโดยใช้ Loop
 
