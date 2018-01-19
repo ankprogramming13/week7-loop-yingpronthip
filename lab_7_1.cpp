4cf36678491a31c1053dbd9ade87aaf2d9ff44d4
Unknown <pronthipza@gmail.com>
2018-01-19 13:56:37 +0700

    ข้อ 1

--- lab_7_1.cpp
+++ lab_7_1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-  
+
   cout << "This is simple loop"<< endl << endl ;
 
   int i = 0;
@@ -12,16 +12,12 @@ int main() {
   for(i = 0; i < 10 ; i++) {
     cout << "Round " << i << endl;
   }
+for(i = 0; i < 10 ; i++) {
+    cout << "Hello World " << i << endl;
+  }
 
   // TODO : แก้ไขส่วนนี้ด้วยการใช้ for
-  cout << "Hello World 1" << endl;
-  cout << "Hello World 2" << endl;
-  cout << "Hello World 3" << endl;
-  cout << "Hello World 4" << endl;
-  cout << "Hello World 5" << endl;
-  cout << "Hello World 6" << endl;
-  cout << "Hello World 7" << endl;
-  cout << "Hello World 8" << endl;
+
 
   return 0;
 }
