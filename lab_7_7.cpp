a5ce82fa724831b2719c33d6c61ec32644dca158
Unknown <pronthipza@gmail.com>
2018-01-19 14:02:11 +0700

    ข้อ 7

--- lab_7_7.cpp
+++ lab_7_7.cpp
@@ -2,9 +2,24 @@
 
 using namespace std;
 
-int main () {
+int main ()
+{
 
-  // เขียนโปรแกรมเพื่อรับค่าตัวเลข 5 จำนวน และหาค่าเฉลี่ย
 
-  return 0;
+    int a = 0;
+    int num, sum=0, avg;
+
+    // while loop execution
+    while( a < 5 )
+    {
+        cout << "Enter" << a + 1 << "Number: ";
+        cin >> num;
+        sum = sum + num;
+        a++;
+    }
+    avg = (float) sum / 5;
+    cout << "Total" << "\t" << avg;
+    // เขียนโปรแกรมเพื่อรับค่าตัวเลข 5 จำนวน และหาค่าเฉลี่ย
+
+    return 0;
 }
