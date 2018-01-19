65daf083c15aac87b17523543cdbcb87e1a00c24
Unknown <pronthipza@gmail.com>
2018-01-19 13:59:44 +0700

    ข้อ 3

--- lab_7_3.cpp
+++ lab_7_3.cpp
@@ -9,10 +9,16 @@ int main() {
   // ตัวอย่างโปรแกรมที่ใช้ for
 
   int i = 0;
-  for (i = 0; i < 10; i+1) {
+  int a = 0;
+
+  for (i = 0; i < 10; i++) {
     cout << "Run from for" << endl;
   }
-  cout << "************END FOR************" << endl;
+  while( a < 10 ) {
+      cout << "Run from while" << endl;
+      a++;
+    }
+  cout << "**********END FOR**********" << endl;
 
   // เขียนโปรแกรมที่ทำงานเหมือนด้านบนโดยใช้ while แทน for
 
